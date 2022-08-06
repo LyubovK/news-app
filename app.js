1d05b0b136c0b19bd8142853ba7862e4d9b77d4c
Liubov <kortcova@gmail.com>
2022-08-06 03:31:45 +0300

    init news-app

---
+++ app.js
@@ -0,0 +1,127 @@
+// Custom Http Module
+function customHttp() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+// Init http module
+// https://newsapi.org/v2/everything?q=bitcoin&apiKey=ef3222971c9e4fb8945c010a17ba996e
+
+const http = customHttp();
+const newsService = (function () {
+  const apiKey = 'ef3222971c9e4fb8945c010a17ba996e';
+  const apiUrl = 'https://newsapi.org/v2';
+
+  return {
+    topHeadLines(country = 'ua', cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`,
+        cb
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    },
+  };
+})();
+//  init selects
+document.addEventListener('DOMContentLoaded', function () {
+  M.AutoInit();
+  loadNews();
+});
+
+//load news function
+
+const loadNews = () => {
+  newsService.topHeadLines('ua', onGetResponse);
+};
+
+//function on get response from server
+
+const onGetResponse = (err, res) => {
+  renderNews(res.articles);
+};
+
+// funct render news
+
+const renderNews = (news) => {
+  const newsContainer = document.querySelector('.news-container .row');
+  let fragment = '';
+
+  news.forEach((newsItem) => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+  // console.log(fragment);
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+};
+
+// news item template function
+
+const newsTemplate = ({ urlToImage, title, url, description }) => {
+  return `
+  <div class="col s12">
+    <div class="card">
+      <div class="card-image">
+        <img src="${urlToImage}">
+        <span>${title || ''}</span>
+      </div>
+      <div class="card-content">
+        <p>${description || ''}</p>
+      </div>
+      <div class="card-action">
+        <a href="${url}">Read more</a>
+      </div>
+    </div>
+  </div>
+  `;
+};
